6fb9f1f1d885e34386c1a99491cb1f183aca6bc3
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-27 16:00:22 +0100

    remove __NEXT_BUILD_ID for edge functions

--- crates/next-core/src/next_config.rs
+++ crates/next-core/src/next_config.rs
@@ -1827,6 +1827,11 @@ impl NextConfig {
     }
 
     #[turbo_tasks::function]
+    pub fn has_deployment_id(&self) -> Vc<bool> {
+        Vc::cell(self.deployment_id.is_some())
+    }
+
+    #[turbo_tasks::function]
     pub fn cache_kinds(&self) -> Vc<CacheKinds> {
         let mut cache_kinds = CacheKinds::default();
 
